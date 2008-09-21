1c36e69450f0752b669a7248afb64a45ed882e9b
daevaorn <daevaorn@36a948a3-fc37-0410-8bc3-7137da836232>
2008-09-21 17:26:51 +0000

    remove unneeded branch

--- branches/gen2/setup.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-from distutils.core import setup
-setup( name="Djapian", version="0.9",
-       author='Rafael "SDM" Sierra', author_email="rafaeljsg14@gmail.com",
-       packages=[ "djapian", "djapian.backend" ], package_dir={ "djapian": "src/djapian" },
-       scripts=['src/scripts/run_djapian.py']
-)
--- branches/gen2/src/djapian/backend/backends.py
+++
@@ -1,8 +0,0 @@
-from default import DefaultIndexer
-
-try:
-    from xap import XapianIndexer
-except ImportError:
-    print "Xapian backend will not be available due to an ImportError. " \
-          "Do you have Xapian and Xapwrap installed?"
-
